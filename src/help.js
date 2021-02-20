186fe98b665d211bd4c74abda4f6979963c907b5
swag20199a <76639140+swag20199a@users.noreply.github.com>
2021-02-20 15:24:57 -0400

    Add files via upload

---
+++ src/help.js
@@ -0,0 +1,19 @@
+
+const help = (prefix) => { 
+	return `  
+ COMANDO BOT
+    *${prefix}sticker*
+    Uso: FOTO/GIF/VIDEO (MENOS DE 10S)
+  ESPERO QUE TENHAM GOSTADO DO BOT
+
+
+
+Criador: BETOOOO
+
+`
+}
+exports.help = help
+
+
+
+  
